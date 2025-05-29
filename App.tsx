4da9ff4ea6cdee2226310191c768b7759af8e423
Peter Neumark <neumark.peter@gmail.com>
2025-05-29 13:25:59 +0200

    url handling wip

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
-import TestMDX from './src/testmdx.mdx';
-import {StatefulComponent} from './src/StatefulComponent';
-import {Path, withAsyncComponent} from '@tinkerable/internal/v1';
+import TestMDX from "./src/testmdx.mdx";
+import { StatefulComponent } from "./src/StatefulComponent";
+import { SandboxRouter, withAsyncComponent, Path } from "@tinkerable/internal/v1";
 import { Routes, Route, Link } from "react-router";
-import { Button, HStack } from "@chakra-ui/react"
-import * as React from 'react'
+import { Button, HStack } from "@chakra-ui/react";
+import * as React from "react";
 
 export const Demo = () => {
   return (
@@ -11,31 +11,39 @@ export const Demo = () => {
       <Button>Click me</Button>
       <Button>Click me</Button>
     </HStack>
-  )
-}
+  );
+};
 
 export const App = (): React.JSX.Element => {
-  console.log("Main-content App")
+  //return <>AADFAFAF</>
+
   return (
-    <Routes>
-      <Route index element={<Index />} />
-      <Route path="/index.tsx" element={<Index />} />
-      <Route path="*" element={<Path />} />
-    </Routes>);
-}
+    <SandboxRouter>
+      <Routes>
+        <Route index element={<Index />} />
+        {/* Treat index.tsx as a special case to avoid circular imports */}
+        <Route path="/index.tsx" element={<Index />} />
+        <Route path="*" element={<Path />} />
+      </Routes>
+    </SandboxRouter>
+  );
+
+};
 
 export function Index(): React.JSX.Element {
   const W = withAsyncComponent(
     // @ts-ignore
-    module.dynamicImport('./src/Wrapper')
+    module.dynamicImport("./src/Wrapper")
   );
-  return <>
-    <Demo />
-    <Link to="about">About</Link>
-    <W>ASDF</W>
-    <h1>Hello woorld</h1>
-    <StatefulComponent />
+  return (
+    <>
+      <Demo />
+      <Link to="/src/pages/toc.mdx">TOC</Link>
+      <W>ASDF</W>
+      <h1>Hello woorld</h1>
+      <StatefulComponent />
 
-    <TestMDX />
-  </>
+      <TestMDX />
+    </>
+  );
 }
